731e8fe016cf5226f5621f9c6bfc1c7946eefd9c
BipinChauhan07 <141702727+BipinChauhan07@users.noreply.github.com>
2023-08-27 12:02:14 +0530

    Add files via upload
    Signed-off-by: BipinChauhan07 <141702727+BipinChauhan07@users.noreply.github.com>

---
+++ task 2.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+def generate_password(length=12, include_digits=True, include_special_chars=True):
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_special_chars:
+        characters += string.punctuation
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter the desired password length: "))
+    include_digits = input("Include digits? (y/n): ").lower() == 'y'
+    include_special_chars = input("Include special characters? (y/n): ").lower() == 'y'
+    
+    generated_password = generate_password(password_length, include_digits, include_special_chars)
+    print("Generated Password:", generated_password)
